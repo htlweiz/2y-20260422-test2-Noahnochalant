161572a820ec0f5789d1bba18b77c4a1b4cc8b92
Noahnochalant <gliede240059@sr.htlweiz.at>
2026-04-23 08:56:09 +0000

    Aufgabe 2

--- src/programs/program02/main.c
+++ src/programs/program02/main.c
@@ -8,6 +8,17 @@
 int main(int argc, char **argv) {
   int16_t count = 0;
   // write your solution here
+int number = 0;
+int count = 0;
+do
+{
+  
+  printf("Würfel:\n");
+  scanf("%d",&number);
+  count = number;
+  count += count;
+} while (number < 7);
 
-  printf("Du hast %d Mal gewürfelt.", count);
+ printf("Du hast %d Mal gewürfelt.", count);
+
 }
