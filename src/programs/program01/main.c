c1d55d981bf789e79599d66960bfc17486bf0371
Noahnochalant <gliede240059@sr.htlweiz.at>
2026-04-23 08:47:42 +0000

    Aufgabe 1

--- src/programs/program01/main.c
+++ src/programs/program01/main.c
@@ -7,5 +7,35 @@
 
 int main(int argc, char **argv) {
    // write your solution here
+int number = 0;
+int count = 0;
+printf("Geben Sie eine Zahl ein:\n");
+scanf("%d",&number);
+if (number > 0)
+{
+do
+{
+   printf("Geben Sie eine Zahl ein:\n");
+   scanf("%d",&number);
+   count = number +1 ;
+} while (count != number);
 
+}else{
+   do
+   {
+      printf("Geben Sie eine Zahl ein:\n");
+      scanf("%d",&number);
+      count = number - 1;
+   } while (count != number);
+   
 }
+ 
+ 
+   
+   
+   
+}
+
+
+
+
