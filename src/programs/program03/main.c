57cd7095bca91d08cdc29e9b4b0c2f269e6d8a53
Noahnochalant <gliede240059@sr.htlweiz.at>
2026-04-23 09:22:27 +0000

    test

--- src/programs/program03/main.c
+++ src/programs/program03/main.c
@@ -6,13 +6,25 @@
 #include <stdio.h>
 
 // TODO: your code goes here
+double Celsius2Fahrenheit()
+{
+  double Fahrnheit= 0;
+  double temp_celsius = 0;
+  printf("Temperatur Celsius:\n");
+  Fahrnheit = temp_celsius * (9/5) +32;
+  
+  return Celsius2Fahrenheit;
+  return temp_celsius;
+}
+
 
 int main(void) {
   double temp_celsius = 0;
-
+ 
+  
   scanf("%lf", &temp_celsius);
 
-  printf("%f", Celsius2Fahrenheit(temp_celsius));
+  printf("%lf", Celsius2Fahrenheit(temp_celsius));
 
   return 0;
 }
--- src/programs/program04/main.c
+++ src/programs/program04/main.c
@@ -4,15 +4,24 @@
 
 #include <inttypes.h>
 #include <stdio.h>
-
+#include <math.h>
 // TODO: your code goes here
+double compute_hypotenuse(double leg_a, double leg_b){
+  double hypoth = 0;
+  hypoth =sqrt(pow(leg_a ,2) + pow(leg_b ,2)) ;
+  return compute_hypotenuse;
+
+} 
+
+
 
 int main(void) {
   double leg_a = 0, leg_b = 0;
-
+printf("Geben sie die Länge a ein:\n");
+  printf("Geben sie Länge b ein:\n");
   scanf("%lf", &leg_a);
-
-  printf("%f", compute_hypotenuse(leg_a, leg_b));
+  scanf("%lf", &leg_b);
+  printf("%lf", compute_hypotenuse(leg_a, leg_b));
 
   return 0;
 }
